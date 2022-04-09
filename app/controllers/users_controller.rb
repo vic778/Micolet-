7aa770f885c3f14eb61406c697cb0ea22b26f2d4
vic778 <victoiremmanuelbarh@gmail.com>
2022-04-09 16:30:04 +0300

    Add notifications

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
       UserMailer.newsletter_confirmation(@user).deliver_now
       redirect_to root_path, notice: 'Email sent! Thank you for subscribing to our newsletter! :)'
     else
-      redirect_to root_path
+      redirect_to root_path, alert: 'Email can not be blanck and the preferences should be selected!'
     end
   end
 
